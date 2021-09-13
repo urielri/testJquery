619f596c80e8ba03a09283ba99aff4f2df57c57a
urielri <urielrivps@gmail.com>
2021-09-13 18:18:35 -0300

    fetch

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const button = document.getElementById("buttonJ");
+const button = document.getElementById("buttonJquery");
 let proxy = "https://cors-anywhere.herokuapp.com/";
 button.addEventListener("click", () => {
   $.ajax({
@@ -8,24 +8,46 @@ button.addEventListener("click", () => {
     headers: {
       accept: "application/json",
       "Access-Control-Allow-Credentials": "true",
-      "Access-Control-Allow-Origin": "http://localhost:5500",
     },
     success: function (data) {
-      $(".container").append(
-        `<p>Dolar compra:$ ${data.compra} Dolar venta:$ ${data.venta}<p>`
+      $("#p").append(
+        `<p>Dolar compra: ${data.compra} Dolar venta: ${data.venta}<p>`
       );
     },
   });
 });
-const butt = document.getElementById("buttonF");
-butt.addEventListener("click", () => {
-  fetch("https://api-dolar-argentina.herokuapp.com/api/dolaroficial", {
+const buttonFetch = document.getElementById("buttonFetch");
+buttonFetch.addEventListener("click", () => {
+  fetch("https://rickandmortyapi.com/api/character/1", {
     mode: "cors",
     method: "GET",
     headers: {
       "Content-Type": "application/json",
     },
-  }).then((res) => {
-    console.log(res);
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .then((res) => {
+      $("#p").append(
+        `<p>Nombre personaje: ${res.name} especie: ${res.species}<p>`
+      );
+    });
+  buttonFetch.disabled = true;
+});
+const buttonJqueryM = document.getElementById("buttonJqueryM");
+buttonJqueryM.addEventListener("click", () => {
+  $.ajax({
+    url: "https://rickandmortyapi.com/api/character/1",
+    type: "GET",
+    crossDomain: true,
+    headers: {
+      accept: "application/json",
+      "Access-Control-Allow-Credentials": "true",
+    },
+    success: function (data) {
+      $("#p").append(`<p>Name: ${data.name} Specie: ${data.species}<p>`);
+    },
   });
+  buttonJqueryM.disabled = true;
 });
